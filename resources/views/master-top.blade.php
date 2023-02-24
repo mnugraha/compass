1279748a6fbdfc74cd75e980c991a654208ab090
nugraha8989 <nugraha.muhammad@gmail.com>
2023-02-24 14:00:02 +0700

    icon

--- resources/views/master-top.blade.php
+++ resources/views/master-top.blade.php
@@ -9,8 +9,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <!-- END META SECTION -->
     <!-- CSS INCLUDE -->
     <link rel="stylesheet" href="/theme/css/styles.css">
